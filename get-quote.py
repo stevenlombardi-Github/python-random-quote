acb39da33ac471d6dfa2ee5aa15627e2bcea193e
stevenlombardi-Github <lombardi.steven@gmail.com>
2021-06-18 10:22:48 -0400

    Random 2 line without newline working

--- get-quote.py
+++ get-quote.py
@@ -8,8 +8,12 @@ def primary():
   f.close()
 
   last = len(quotes)-1
-  rnd = random.randint(0,last)
-  print(quotes[rnd])
+  rnd1 = random.randint(0,last)
+  rnd2 = random.randint(0,last)
+#  if '\n' in quotes[rnd1]:
+#       print("there is a newline in here")
+  print(quotes[rnd1].rstrip("\n"))
+  print(quotes[rnd2].rstrip("/n"))
 
 if __name__== "__main__":
   primary()
